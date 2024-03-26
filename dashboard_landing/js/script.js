84b76ffe5ff221f6d2ccf369324777ecba2442da
Enzo Campão Dutra <128257870+enzocdutra@users.noreply.github.com>
2024-03-26 09:38:39 -0300

    Add files via upload

---
+++ dashboard_landing/js/script.js
@@ -0,0 +1,9 @@
+
+    function closeNav(){
+        document.getElementById("myNav").style.width = '0';
+    }
+    function openNav(){
+        document.getElementById("myNav").style.display = 'flex';
+        document.getElementById("myNav").style.width = '55px';
+   
+        }
