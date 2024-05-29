c33185f7751d89a6439dfb365b03165cd8fafcc3
Enzo Campão Dutra <128257870+enzocdutra@users.noreply.github.com>
2024-05-29 11:01:58 -0300

    Delete dashboard_landing directory

--- dashboard_landing/js/script.js
+++
@@ -1,9 +0,0 @@
-
-    function closeNav(){
-        document.getElementById("myNav").style.width = '0';
-    }
-    function openNav(){
-        document.getElementById("myNav").style.display = 'flex';
-        document.getElementById("myNav").style.width = '55px';
-   
-        }
